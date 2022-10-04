2fcb20e88b08af6fd123c9177c70a176a8cab49c
Demir Boraj <demirboraj97@gmail.com>
2022-10-04 17:15:44 +0200

    1

--- Cat.java
+++ Cat.java
@@ -3,18 +3,17 @@ public class Cat {
     public Integer age;
     public Boolean likesCuddles;
 
-    public Cat(String name, Integer age, Boolean likesCuddles){
-this.name = name;
-this.age = age;
-this.likesCuddles = likesCuddles;
+    public Cat(String name, Integer age, Boolean likesCuddles) {
+        this.name = name;
+        this.age = age;
+        this.likesCuddles = likesCuddles;
     }
 
-    public void displayInfo(){
+    public void displayInfo() {
         if (this.likesCuddles) {
             System.out.printf("This is %s, who is %d years old. %s likes cuddles!", this.name, this.age, this.name);
         } else {
-            System.out.printf("This is %s, who is %d years old. %s doesn't like cuddles!", this.name, this.age,
-                    this.name);
+            System.out.printf("This is %s, who is %d years old. %s doesn't like cuddles!", this.name, this.age, this.name);
 
         }
     }
